492687a27ae440c1d451fc090469d2d4bb8f7c62
MartinK <Martin.klleci@gmail.com>
2024-05-16 10:26:52 +0200

    commit 2

---
+++ cmake-build-debug/CMakeFiles/sistem_librarie_C.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/sistem_biblioteke_C/main.c" "CMakeFiles/sistem_librarie_C.dir/main.c.obj" "gcc" "CMakeFiles/sistem_librarie_C.dir/main.c.obj.d"
+  "F:/sistem_biblioteke_C/sistemi_librat.c" "CMakeFiles/sistem_librarie_C.dir/sistemi_librat.c.obj" "gcc" "CMakeFiles/sistem_librarie_C.dir/sistemi_librat.c.obj.d"
+  "F:/sistem_biblioteke_C/sistemi_studentet.c" "CMakeFiles/sistem_librarie_C.dir/sistemi_studentet.c.obj" "gcc" "CMakeFiles/sistem_librarie_C.dir/sistemi_studentet.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sistem_librarie_C.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sistem_librarie_C.dir/main.c.obj"
+  "CMakeFiles/sistem_librarie_C.dir/main.c.obj.d"
+  "CMakeFiles/sistem_librarie_C.dir/sistemi_librat.c.obj"
+  "CMakeFiles/sistem_librarie_C.dir/sistemi_librat.c.obj.d"
+  "CMakeFiles/sistem_librarie_C.dir/sistemi_studentet.c.obj"
+  "CMakeFiles/sistem_librarie_C.dir/sistemi_studentet.c.obj.d"
+  "libsistem_librarie_C.dll.a"
+  "sistem_librarie_C.exe"
+  "sistem_librarie_C.exe.manifest"
+  "sistem_librarie_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sistem_librarie_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
